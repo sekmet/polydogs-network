d4da47042049fdd5a5b638288ff252d876d8edf5
Carlos Guimaraes <sekmet@open4g.com>
2022-03-27 12:45:09 -0300

    updates in progress

---
+++ src/pages/dogsuprise.tsx
@@ -0,0 +1,34 @@
+import { useEffect, useState } from 'react';
+
+import { useEthers, useBlockMeta, useBlockNumber } from '@usedappify/core';
+
+import { Dashboard } from '@/layouts/Dashboard';
+import { Meta } from '@/layouts/Meta';
+
+const DogSurprise = () => {
+  // const router = useRouter();
+  const [blockNumber, setBlockNumber] = useState<number | undefined>(0);
+  const { account } = useEthers();
+  const { timestamp } = useBlockMeta();
+  const blocknumber = useBlockNumber();
+
+  useEffect(() => {
+    setBlockNumber(blocknumber);
+  }, [blocknumber]);
+
+  return (
+    <Dashboard
+      auth={true}
+      meta={
+        <Meta
+          title="Dog surprise - Polydogs Network"
+          description="A decentralized social network to connect dog breeders/owners/lovers and more..."
+        />
+      }
+    >
+      <div className="max-w-none"></div>
+    </Dashboard>
+  );
+};
+
+export default DogSurprise;
